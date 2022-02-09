8d89ed7d3c4a95f1c1a37df409a32cc71589eb27
Jonas Paro <jonas.paro@omegapoint.se>
2022-02-09 19:54:48 +0100

    Checkbox bind to group

--- src/RecruitHero.svelte
+++ src/RecruitHero.svelte
@@ -2,14 +2,21 @@
     let name;
     let power;
 
+    let abilities = [];
+
     const handleSubmit = () => {
-        console.log(name, power);
+        console.log(name, power, abilities);
     }
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
     <input type="text" placeholder="name" bind:value={name}>
     <input type="text" placeholder="power" bind:value={power}>
+    <label>Abilities</label>
+    <input type="checkbox" bind:group={abilities} value="Super strength"> Super strength<br>
+    <input type="checkbox" bind:group={abilities} value="Flight"> Flight<br>
+    <input type="checkbox" bind:group={abilities} value="Invisibility"> Invisibility<br>
+    <input type="checkbox" bind:group={abilities} value="Laser"> Laser<br>
     <button>Recruit hero</button>
 </form>
 
